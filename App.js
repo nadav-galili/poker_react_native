20b4c113049be8ffd50bcd55ae07c03ea5472602
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2022-07-03 16:01:50 +0300

    added tabnavigator

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
@@ -14,22 +16,51 @@ import MyTeams from "./app/screens/MyTeams";
 import AccountScreen from "./app/screens/AccountScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import SignInScreen from "./app/screens/SignInScreen";
-import Screen from "./app/components/Screen";
 
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: colors.primaryPurple },
+      headerTintColor: colors.white,
+      // headerShown: false,
+    }}
+  >
+    <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    <Stack.Screen name="SignInScreen" component={SignInScreen} />
+    <Stack.Screen name="AccountScreen" component={AccountScreen} />
+    <Stack.Screen name="MyTeams" component={MyTeams} />
+    {/* <Stack.Screen name="MyTeams" component={MyTeams} options={({route})=>({title:route.params.id})} /> */}
+  </Stack.Navigator>
+);
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintcolor: "white",
+      headerShown: false,
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={WelcomeScreen}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="fff" component={SignInScreen} />
+  </Tab.Navigator>
+);
 export default function App() {
-  const Stack = createStackNavigator();
-  const StackNavigator = () => (
-    <Stack.Navigator>
-      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="SignInScreen" component={SignInScreen} />
-      <Stack.Screen name="AccountScreen" component={AccountScreen} />
-      <Stack.Screen name="MyTeams" component={MyTeams} />
-    </Stack.Navigator>
-  );
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
